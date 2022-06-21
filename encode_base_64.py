b374694489b0fb6b5a01edba7caae1bf7e9e842a
Claire <claira.m@live.fr>
2022-06-21 16:13:15 +0200

    metrics CPU, Memory, cli with args

--- encode_base_64.py
+++ encode_base_64.py
@@ -1,28 +1,42 @@
 import argparse
+import step2
 
 
-parser = argparse.ArgumentParser()
-# parser.parse_args()
-parser.add_argument("year")
-args = parser.parse_args()
-print(args.year)
+def get_user_string_in_args():
+    parser = argparse.ArgumentParser()
+    # parser.parse_args()
+    # parser.add_argument("string", help="letters", type=str)
+    # parser.add_argument("echo",  help="echo the string you enter")
 
+    parser.add_argument("-t", "--text_to_encode", help="encode in base 64", type=str)
+    # parser.add_argument("-n", "--number", help="encode in base 64", type=int)
+    args = parser.parse_args()
+    print(args.text_to_encode)
+    user_string_input = args.text_to_encode
+    return user_string_input
 
-def get_input():
+    # def get_input():
     """
      Function getting the user input
     :return: the user input
     :rtype: a string
     """
-    user_input = input("enter a string : ")
-    try:
-        str(user_input)
-    except:
-        print("Houston, we've got a problem.")
-
-    else:
-        print("I think it is working. The user entered : " + user_input)
-    return user_input
 
 
-get_input()
+#     user_input = input("enter a string : ")
+#     try:
+#         str(user_input)
+#     except:
+#         print("Houston, we've got a problem.")
+#
+#     else:
+#         print("I think it is working. The user entered : " + user_input)
+#     return user_input
+#
+#
+# get_input()
+
+
+# get_user_string_in_args()
+
+step2.step2(get_user_string_in_args())
---
+++ tests_psutil.py
@@ -0,0 +1,20 @@
+import psutil
+
+# CPU
+print(psutil.cpu_times(percpu=False))
+print(psutil.cpu_times())
+print(psutil.cpu_times_percent(interval=None, percpu=False))
+print(psutil.cpu_count(logical=True))
+# print(len(psutil.Process().cpu_affinity()))
+print(psutil.cpu_stats())
+print(psutil.cpu_freq(percpu=False))
+print(psutil.getloadavg())
+
+# MEMORY
+print(psutil.virtual_memory())
+print(psutil.swap_memory())
+
+# DISK
+print(psutil.disk_partitions(all=False))
+# print(psutil.disk_usage(path))
+
